90b97d3cfd8ee1e74a1b42016e8f867a540c3f6d
Ali Raza Alvi <aliraza432@gmail.com>
2016-11-06 19:59:25 +0400

    Resolved jsx extension in webpack.config.

--- app/Index.jsx
+++ app/Index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Store from './stores/Index';
-import Main from './components/Main.jsx';
+import Main from './components/Main';
 
 ReactDOM.render(<Provider store={Store}><Main /></Provider>, document.getElementById('content'));
